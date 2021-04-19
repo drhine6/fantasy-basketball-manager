44f47eb682f3cc485a14da08efef60adbdab02b3
David Rhine <drhine6@gmail.com>
2021-04-19 11:25:16 -0700

    Pull player stats on search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import PlayerSearch from "./components/PlayerSearch";
+import PlayerTable from "./components/PlayerTable";
+
+const API_ENDPOINT = "https://www.balldontlie.io/api/v1/stats";
 
 function App() {
+  const [players, setPlayers] = useState([]);
+
+  const addPlayer = async (player) => {
+    console.log(player);
+    const url = `${API_ENDPOINT}?seasons[]=2020&player_ids[]=${player.id}`;
+    console.log(url);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <div className="App">
-      <PlayerSearch />
+      <PlayerSearch addPlayer={addPlayer} />
+      <PlayerTable players={players} />
     </div>
   );
 }
--- frontend/src/components/PlayerSearch.js
+++ frontend/src/components/PlayerSearch.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
+import Typography from "@material-ui/core/Typography";
 
 const API_ENDPOINT = "https://www.balldontlie.io/api/v1/players";
 
-export default function PlayerSearch() {
+export default function PlayerSearch(props) {
   const [playerName, setPlayerName] = useState("");
   const [players, setPlayers] = useState([]);
+
   useEffect(() => {
     const getPlayers = async () => {
       const encoded = playerName.replace(" ", "%20");
       const url = `${API_ENDPOINT}?search=${encoded}`;
+
       fetch(url)
         .then((response) => response.json())
         .then((data) => {
@@ -21,20 +24,21 @@ export default function PlayerSearch() {
       getPlayers();
     }
   }, [playerName]);
+
   return (
     <Autocomplete
       id="player-search"
       value={playerName}
       onChange={(event, newValue) => {
-        setPlayerName(newValue);
+        props.addPlayer && props.addPlayer(newValue);
+        const playerName = `${newValue.first_name} ${newValue.last_name}`;
+        setPlayerName(playerName);
       }}
       inputValue={playerName}
       onInputChange={(event, newValue) => {
         setPlayerName(newValue);
       }}
-      options={players.map(
-        (player) => `${player.first_name} ${player.last_name}`
-      )}
+      options={players}
       renderInput={(params) => (
         <TextField
           {...params}
@@ -43,6 +47,14 @@ export default function PlayerSearch() {
           variant="outlined"
           InputProps={{ ...params.InputProps, type: "search" }}
         />
+      )}
+      getOptionLabel={(option) =>
+        typeof option === "string" ? option : option.first_name
+      }
+      renderOption={(player) => (
+        <Typography noWrap>
+          {player.first_name} {player.last_name}
+        </Typography>
       )}
       disableClearable
     />
